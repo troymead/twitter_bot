8665f29d47bcaf26e8c8932255a330d7fb9b3273
troymead <tspringmeadows@gmail.com>
2021-01-09 17:49:21 -0500

    changed to use clock worker

--- bot_final.py
+++ bot_final.py
@@ -5,6 +5,7 @@ import random
 import tweepy
 from os import environ
 from bot_functions import get_random_song, get_tweet
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # for heroku integration
 CONSUMER_KEY = environ['CONSUMER_KEY']
@@ -15,19 +16,39 @@ ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
 # Time interval for tweets (one per day - every 24 hours)
 # INTERVAL = 60 * 60 * 24
 INTERVAL = 60 * 30 # heroku deploy test interval
+INTERVAL2 = 60
 
 # set up OAuth and integrate with API; twitter test
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 tweepy_api = tweepy.API(auth)
 
-not_empty = True
+# not_empty = True
 
-while not_empty:
+sched = BlockingScheduler()
+
+# @sched.scheduled_job('cron', day_of_week='', hour=)
+# for testing:
+@sched.scheduled_job('interval', minutes=5)
+def once_day():
     print('Preparing to retrieve song to tweet...')
-
     song = get_tweet(get_random_song())
     not_empty = song[0]
-    tweepy_api.update_status(song[1]) # for final
-    if not_empty:
-        time.sleep(INTERVAL)
+    print(song[1])
+
+    if (not_empty == False):
+        sched.shutdown(wait=False)
+
+sched.start()
+
+# while not_empty:
+#     print('Preparing to retrieve song to tweet...')
+
+#     song = get_tweet(get_random_song())
+#     not_empty = song[0]
+#     # tweepy_api.update_status(song[1]) # for final
+
+
+
+#     if not_empty:
+#         time.sleep(INTERVAL)
