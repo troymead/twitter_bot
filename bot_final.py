e429237d919d2af274ab3a544c5dd1bba156e220
troymead <tspringmeadows@gmail.com>
2021-01-10 20:07:45 -0500

    hopefully last test to get empty string to tweet

--- bot_final.py
+++ bot_final.py
@@ -14,48 +14,24 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
 
-# Time interval for tweets (one per day - every 24 hours)
-# INTERVAL = 60 * 60 * 24
-INTERVAL = 60 * 30 # heroku deploy test interval
-INTERVAL2 = 60
-
 # set up OAuth and integrate with API; twitter test
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 tweepy_api = tweepy.API(auth)
 
-# not_empty = True
-
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('cron', day_of_week='', hour=)
 # for testing:
-@sched.scheduled_job('interval', hours=1, start_date=datetime.now())
+# @sched.scheduled_job('cron', start_date='2021-01-11', hour='10,22', timezone='America/New York')
+@sched.scheduled_job('interval', minutes=5, start_date=datetime.now())
 def once_day():
     print('Preparing to retrieve song to tweet...')
     song = get_tweet(get_random_song())
     not_empty = song[0]
-    print(song[1])
 
-    if (not_empty):
-        print(song[1])
-        tweepy_api.update_status(song[1]) # for final
-        # sched.shutdown(wait=False)
-    else:
-        print(song[1])
-        tweepy_api.update_status(song[1]) # for final
-        sched.shutdown(wait=False)
+    tweepy_api.update_status(song[1])
+
+    if (not_empty == False):
+        sched.shutdown()
 
 sched.start()
-
-# while not_empty:
-#     print('Preparing to retrieve song to tweet...')
-
-#     song = get_tweet(get_random_song())
-#     not_empty = song[0]
-#     # tweepy_api.update_status(song[1]) # for final
-
-
-
-#     if not_empty:
-#         time.sleep(INTERVAL)
--- bot_functions.py
+++ bot_functions.py
@@ -9,12 +9,11 @@ SPOTIFY_CLIENT_ID = environ['SPOTIFY_CLIENT_ID']
 SPOTIFY_CLIENT_SECRET = environ['SPOTIFY_CLIENT_SECRET']
 username = 'spotify:user:1250284673'.split(':')[2]
 scope = 'playlist-modify-public'
-# redirect_uri = REDIRECT_URI
 redirect_uri = environ['REDIRECT_URI']
 
 def get_random_song():
-    # playlist_id = 'spotify:playlist:4CneB3XScAKgQseSXey2Yx'.split(':')[2]
-    # test playlist
+    # final playlist - twitter bot (redux)
+    # playlist_id = 'spotify:playlist:2AVp8hX9Xaiqg9xv8qp68v'.split(':')[2]
     playlist_id = 'spotify:playlist:7B0w4UowILfFDuWJQvwC6c'.split(':')[2]
     at_id = 'spotify:playlist:3b64drC4E4qkcmiOs3cJaQ'.split(':')[2]
 
@@ -22,11 +21,10 @@ def get_random_song():
                                                            client_secret=SPOTIFY_CLIENT_SECRET,
                                                            redirect_uri=redirect_uri,
                                                            scope=scope
-                                                           )) # need to test with heroku
+                                                           ))
 
     twitter_playlist = sp.playlist_items(playlist_id)
     
-
     if twitter_playlist['total'] == 0:
         return [False]
     else:
